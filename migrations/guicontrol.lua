41347efba9e8456defc0ef5af5762f091ed217c2
d3x0r <d3x0r@users.noreply.github.com>
2017-07-27 17:58:02 -0700

    Add migration code; may delete the other upgrade planner's gui.

---
+++ migrations/guicontrol.lua
@@ -0,0 +1,32 @@
+
+require("mod-gui")
+
+
+local player = game.players[1];
+if( player ) then 
+    if player.gui.top["replacer-config-button"] then
+        player.gui.top["replacer-config-button"].destroy() 
+    end
+    if player.gui.top["upgrade-planner-config-button"] then
+      player.gui.top["upgrade-planner-config-button"].destroy()
+    end
+    if player.gui.left["upgrade-planner-config-frame"] then
+      player.gui.left["upgrade-planner-config-frame"].destroy()
+    end
+    
+    if player.gui.left["upgrade-planner-storage-frame"] then
+      player.gui.left["upgrade-planner-storage-frame"].destroy()
+    end
+
+    local flow = mod_gui.get_button_flow(player)
+    if flow["upgrade-planner-config-button"] then
+    	flow["upgrade-planner-config-button"].destroy();
+     end;
+    if flow["upgrade-planner2-config-frame"] then
+    	flow["upgrade-planner-config-button"].destroy();
+     end;
+
+end;
+
+
+
